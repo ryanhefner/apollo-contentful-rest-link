ded8ccc8ddf176681fa20586414a1608ab317a77
Ryan Hefner <hi@ryanhefner.com>
2020-08-13 08:03:12 -0400

    Convert query variables to formats that are supported by Contentful REST API

--- src/index.js
+++ src/index.js
@@ -18,26 +18,50 @@ export default class ContentfulRestLink extends ApolloLink {
   }
 
   request(operation, forward) {
-    const { query, variables } = operation
+    const { query, variables, operationName } = operation
 
     const obs = forward
       ? foward(operation)
       : Observable.of({ data: {} })
 
-    // Find operationName to apply to root property of the GraphQL data response
-    const operationName = query.definitions
+    // Find name to apply as root field of the GraphQL data
+    const rootField = query[operationName].definitions
       .find(definition => definition.operation === 'query')
       ?.selectionSet.selections
       .find(selection => selection.name.kind === 'Name')
       ?.name.value;
 
+    // Convert the query variables to a format the Contentful API understands
+    const parseQueryVariables = (query, variables) => {
+      const operationVariables = query[operationName].definitions
+        .find(definition => definition.operation === 'query')
+        ?.variableDefinitions.map(variableDefinition => variableDefinition.variable.name.value)
+
+      const operationQueries = Object.keys(variables)
+        .filter(variableKey => operationVariables.includes(variableKey))
+        .map(variableKey => {
+          // @todo Confirm if [in] is the proper default to apply to these - Ryan
+          const queryKey = `fields.${variableKey}[in]`
+          return { [queryKey]: variables[variableKey] }
+        })
+        .reduce((acc, cur) => {
+          return {...acc, ...cur }
+        }, {});
+
+      return { ...operationQueries, ...omit(variables, operationVariables) }
+    }
+
     return obs.flatMap(({ data, errors }) => new Observable(observer => {
       const queryMethod = variables.hasOwnProperty('id')
         ? 'getEntry'
         : 'getEntries'
-      const queryArgs = operationName.endsWith('Collection')
-        ? [{ ...variables, ...this.queryDefaults, content_type: operationName.replace('Collection', '')}]
-        : [variables.id, { ...this.queryDefaults, ...omit(variables, ['id']) }]
+      const queryArgs = variables.hasOwnProperty('id')
+        ? [variables.id, { ...this.queryDefaults, ...parseQueryVariables(query, omit(variables, ['id'])) }]
+        : [{
+            ...parseQueryVariables(query, variables),
+            ...this.queryDefaults,
+            content_type: rootField.replace('Collection', '')
+          }]
 
       // Contentful query
       this.client[queryMethod](...queryArgs)
@@ -46,7 +70,7 @@ export default class ContentfulRestLink extends ApolloLink {
           graphql(
             contentfulResolver,
             query,
-            graphqlParser(operationName, contentfulData),
+            graphqlParser(rootField, contentfulData),
           )
             .then(data => {
               observer.next({ data, errors })
